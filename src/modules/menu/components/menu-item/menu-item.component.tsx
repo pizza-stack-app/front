4c857fd62caa28ee1e860b79e28660d6881ea33d
Andrii Rastorhuiev <arastorhuiev@gmail.com>
2022-10-01 21:45:29 +0300

    fix alt in menu-item component.

--- src/modules/menu/components/menu-item/menu-item.component.tsx
+++ src/modules/menu/components/menu-item/menu-item.component.tsx
@@ -30,7 +30,7 @@ export const MenuItem: FC<MenuItemProps> = ({
         <img
           className='w-full h-[15rem] object-cover object-center rounded-t-2xl'
           src={imagePath}
-          alt='pizza'
+          alt={title}
         />
         <span className='absolute bottom-1.5 right-3 bg-gray-900/50 text-white text-sm px-2 rounded-[2rem]'>
           {weight} г
