38c52d30b19d128237a97afe103e86231b8e6729
Andrii Rastorhuiev <arastorhuiev@gmail.com>
2022-10-01 18:21:07 +0300

    add props to menulist

--- src/modules/menu/components/menu-list/menu-list.story.tsx
+++ src/modules/menu/components/menu-list/menu-list.story.tsx
@@ -1,11 +1,15 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { MenuList } from './menu-list.component';
+import pizzaMenu from '@app/mocks/pizza.json'
 
 export default {
   title: 'Menu/Menu List',
   component: MenuList,
 } as ComponentMeta<typeof MenuList>;
 
-const Template: ComponentStory<typeof MenuList> = () => <MenuList />;
+const Template: ComponentStory<typeof MenuList> = (args) => <MenuList {...args} />;
 
 export const View = Template.bind({});
+View.args = {
+  items: pizzaMenu
+}
